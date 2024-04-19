e970409df4441d6e8010ce7b52cff76c9a164106
alex <windev@nate.com>
2024-04-19 13:20:34 +0900

    png 출력 구현

--- PoEngineSource/Animation.cpp
+++ PoEngineSource/Animation.cpp
@@ -5,6 +5,7 @@
 #include "Animator.h" 
 #include "Renderer.h" 
 #include "Camera.h" 
+#include "Texture.h" 
 
 namespace Bx
 {
@@ -61,20 +62,36 @@ namespace Bx
 		}
 		*/
 
-		//알파블렌드를 쓰려면 해당 이미지에 알파채널이 있어야 한다. 
-		BLENDFUNCTION func{}; 
-		func.BlendOp = AC_SRC_OVER; 
-		func.BlendFlags = 0; 
-		func.AlphaFormat = AC_SRC_ALPHA; 
-		func.SourceConstantAlpha = 255;  //0=>transparent, 255=>Opaque 
+		Sprite sprite = aniSheet[index];
 
-		Sprite sprite = aniSheet[index]; 
-		HDC imghdc = texture->GetHDC(); 
+		Texture::TextureType tt = texture->GetTextureType(); 
+		if (tt == Texture::TextureType::PNG)
+		{
+			Gdiplus::ImageAttributes imgAttrib{}; 
+			imgAttrib.SetColorKey(Gdiplus::Color(100, 100, 100), Gdiplus::Color(255, 255, 255)); 
+			Gdiplus::Graphics graphics(_hdc);
+			graphics.DrawImage(texture->GetImage(), Gdiplus::Rect(pos.x, pos.y, sprite.tSpan.x, sprite.tSpan.y), 
+				sprite.tLeftTop.x, sprite.tLeftTop.y,
+				sprite.tLeftTop.x + sprite.tSpan.x, sprite.tLeftTop.y + sprite.tSpan.y, 
+				Gdiplus::UnitPixel, nullptr
+				);
+		}
+		else if (tt == Texture::TextureType::BMP)
+		{
+			//알파블렌드를 쓰려면 해당 이미지에 알파채널이 있어야 한다. 
+			BLENDFUNCTION func{};
+			func.BlendOp = AC_SRC_OVER;
+			func.BlendFlags = 0;
+			func.AlphaFormat = AC_SRC_ALPHA;
+			func.SourceConstantAlpha = 100;  //0=>transparent, 255=>Opaque 
+			
+			HDC imghdc = texture->GetHDC();
 
-		AlphaBlend(_hdc, pos.x, pos.y, 
-			sprite.tSpan.x * 3, sprite.tSpan.y * 3, imghdc, 
-			sprite.tLeftTop.x, sprite.tLeftTop.y, 
-			sprite.tSpan.x, sprite.tSpan.y, func);
+			AlphaBlend(_hdc, pos.x, pos.y,
+				sprite.tSpan.x * 3, sprite.tSpan.y * 3, imghdc,
+				sprite.tLeftTop.x, sprite.tLeftTop.y,
+				sprite.tSpan.x, sprite.tSpan.y, func);
+		}
 	}
 
 	void Animation::CreateAnimation(const std::wstring& _name, Texture* _spriteSheet, 

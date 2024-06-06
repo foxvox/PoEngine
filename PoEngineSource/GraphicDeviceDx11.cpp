3f75b21c2521356b77a55eeb00893f9c7419181c
alex <windev@nate.com>
2024-06-07 04:55:45 +0900

    vertex pixel buffer

--- PoEngineSource/GraphicDeviceDx11.cpp
+++ PoEngineSource/GraphicDeviceDx11.cpp
@@ -26,23 +26,23 @@ namespace Bx
 		DXGI_SWAP_CHAIN_DESC swapChainDesc{}; 
 		swapChainDesc.OutputWindow = app.GetHWND();
 		//창모드로 실행할지 선택
-		swapChainDesc.Windowed = true; 
+		swapChainDesc.Windowed	  = true; 
 		swapChainDesc.BufferCount = 2; 
 		//Resize 실행을 가능하게 해준다. 
-		swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; 
-		swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+		swapChainDesc.Flags		  = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; 
+		swapChainDesc.SwapEffect  = DXGI_SWAP_EFFECT_DISCARD;
 		swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; 
 		//창크기 지정 
-		swapChainDesc.BufferDesc.Width = app.GetWidth(); 
+		swapChainDesc.BufferDesc.Width  = app.GetWidth(); 
 		swapChainDesc.BufferDesc.Height = app.GetHeight(); 
 		//픽셀의 데이터 크기 지정 
 		swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM; 
 		//최대 fps 
-		swapChainDesc.BufferDesc.RefreshRate.Numerator = 144; 
+		swapChainDesc.BufferDesc.RefreshRate.Numerator		= 144; 
 		//denominator 분모 
-		swapChainDesc.BufferDesc.RefreshRate.Denominator = 1; 
-		swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED; 
-		swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; 
+		swapChainDesc.BufferDesc.RefreshRate.Denominator	= 1; 
+		swapChainDesc.BufferDesc.Scaling					= DXGI_MODE_SCALING_UNSPECIFIED; 
+		swapChainDesc.BufferDesc.ScanlineOrdering			= DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED; 
 
 		//4X MSAA supported check
 		UINT quality = 0; 
@@ -60,12 +60,12 @@ namespace Bx
 		}*/ 
 
 		//임시로 else part 사용 
-		swapChainDesc.SampleDesc.Count = 1;
+		swapChainDesc.SampleDesc.Count	 = 1;
 		swapChainDesc.SampleDesc.Quality = 0;
 
-		Microsoft::WRL::ComPtr<IDXGIDevice>		dxgiDevice = nullptr;
-		Microsoft::WRL::ComPtr<IDXGIAdapter>	adapter = nullptr;
-		Microsoft::WRL::ComPtr<IDXGIFactory>	factory = nullptr; 
+		Microsoft::WRL::ComPtr<IDXGIDevice>		dxgiDevice  = nullptr;
+		Microsoft::WRL::ComPtr<IDXGIAdapter>	adapter		= nullptr;
+		Microsoft::WRL::ComPtr<IDXGIFactory>	factory		= nullptr; 
 
 		if (FAILED(device->QueryInterface(__uuidof(IDXGIDevice), (void**)dxgiDevice.GetAddressOf())))  
 			return; 
@@ -84,13 +84,13 @@ namespace Bx
 		device->CreateRenderTargetView(renderTarget.Get(), nullptr, renderTargetView.GetAddressOf()); 
 
 		D3D11_TEXTURE2D_DESC depthStencilDesc{}; 
-		depthStencilDesc.BindFlags = D3D11_BIND_FLAG::D3D11_BIND_DEPTH_STENCIL; 
-		depthStencilDesc.Format = DXGI_FORMAT::DXGI_FORMAT_D24_UNORM_S8_UINT;
-		depthStencilDesc.Usage = D3D11_USAGE::D3D11_USAGE_DEFAULT;
-		depthStencilDesc.Width = app.GetWidth(); 
-		depthStencilDesc.Height = app.GetHeight(); 
-		depthStencilDesc.ArraySize = 1; 
-		depthStencilDesc.SampleDesc.Count = 1; 
+		depthStencilDesc.BindFlags			= D3D11_BIND_FLAG::D3D11_BIND_DEPTH_STENCIL; 
+		depthStencilDesc.Format				= DXGI_FORMAT::DXGI_FORMAT_D24_UNORM_S8_UINT;
+		depthStencilDesc.Usage				= D3D11_USAGE::D3D11_USAGE_DEFAULT;
+		depthStencilDesc.Width				= app.GetWidth(); 
+		depthStencilDesc.Height				= app.GetHeight(); 
+		depthStencilDesc.ArraySize			= 1; 
+		depthStencilDesc.SampleDesc.Count	= 1; 
 		depthStencilDesc.SampleDesc.Quality = 0; 
 
 		//깊이버퍼, 깊이버퍼뷰 생성
@@ -121,6 +121,31 @@ namespace Bx
 
 			device->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &psShader); 
 		}
+
+		D3D11_INPUT_ELEMENT_DESC inputLayoutDesces[2] = {}; 
+
+		inputLayoutDesces[0].AlignedByteOffset  = 0; 
+		inputLayoutDesces[0].Format				= DXGI_FORMAT_R32G32B32_FLOAT; 
+		inputLayoutDesces[0].InputSlot			= 0;
+		inputLayoutDesces[0].InputSlotClass		= D3D11_INPUT_PER_VERTEX_DATA; 
+		inputLayoutDesces[0].SemanticName		= "POSITION"; 
+		inputLayoutDesces[0].SemanticIndex		= 0; 
+
+		inputLayoutDesces[1].AlignedByteOffset	= 12;
+		inputLayoutDesces[1].Format				= DXGI_FORMAT_R32G32B32A32_FLOAT;
+		inputLayoutDesces[1].InputSlot			= 0;
+		inputLayoutDesces[1].InputSlotClass		= D3D11_INPUT_PER_VERTEX_DATA;
+		inputLayoutDesces[1].SemanticName		= "COLOR"; 
+		inputLayoutDesces[1].SemanticIndex		= 0;
+
+		device->CreateInputLayout(inputLayoutDesces, 1, vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), &inputLayouts); 
+
+		D3D11_BUFFER_DESC bufferDesc{}; 
+
+		bufferDesc.ByteWidth		= sizeof(Vertex) * 3; 
+		bufferDesc.BindFlags		= D3D11_BIND_FLAG::D3D11_BIND_VERTEX_BUFFER; 
+		bufferDesc.Usage			= D3D11_USAGE::D3D11_USAGE_DYNAMIC; 
+		bufferDesc.CPUAccessFlags	= D3D11_CPU_ACCESS_FLAG::D3D11_CPU_ACCESS_WRITE; 
 	}
 
 	void GraphicDeviceDx11::Draw()
--- ShaderSrc/SpritePS.hlsl
+++ ShaderSrc/SpritePS.hlsl
@@ -5,14 +5,14 @@ struct VS_Input
 {
     float3 pos : POSITION;
     float4 color : COLOR;
-    float2 uv : TEXCOORD;
+    //float2 uv : TEXCOORD;
 };
 
 struct VS_Output
 {
     float4 pos : SV_Position;
     float4 color : COLOR;
-    float2 uv : TEXCOORD;
+    //float2 uv : TEXCOORD;
 };
 
 float4 main(VS_Output input) : SV_Target
